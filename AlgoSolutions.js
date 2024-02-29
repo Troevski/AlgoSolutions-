ac4d2737d3e392897a4cd7104aae45224533966c
Troevski <troevski1@mail.ru>
2024-02-29 22:37:33 +0100

    palindrome added

--- AlgoSolutions.js
+++ AlgoSolutions.js
@@ -34,7 +34,49 @@ console.log(getCash([1, 2, 5, 10, 20, 50], 0)); // ['some tekst']
 
 
 
-// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+// 2) isPalindrome
 
+
+const isPalindrome = (str) => {
+   
+    str = str.toLowerCase().replace(/[^a-zа-я0-9]/gi, '');
+    
+    let objCounter = {};
+    let totalObjNums = 0;
+    
+    for(let i = 0; i < str.length; i++){
+      let letter = str[i];
+      
+      if(!objCounter[letter]){
+        objCounter[letter] = 1
+      }else{
+        objCounter[letter] ++
+      }
+    }
+    
+    for(let key in objCounter){
+      if(objCounter[key] % 2 !== 0){
+        totalObjNums++
+          if(totalObjNums > 1) return false
+      }
+    }
+    
+    return true
+  }
+  
+  console.log(isPalindrome('0_0 (: /-\ :) 0–0')) // true
+  console.log(isPalindrome('My age is 0, 0 si ega ym.')) // true
+  console.log(isPalindrome('топот')); // trueee
+  console.log(isPalindrome('л')); // true
+  console.log(isPalindrome('абвгвбж')); //false
+  console.log(isPalindrome('А муза рада музе без ума да разума')); // trueee
+  console.log(isPalindrome('а роза упала на лапу Азора')); // true
+  console.log(isPalindrome('1')); //true
+  console.log(isPalindrome('91019')); //true
+  console.log(isPalindrome('22.02.2022')); //true
+  
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
